adb34dfe0337b3dd9ddc1e36c0a43f821139fb7a
Your NameMegha <you@example.coaamimeghajoy@gmail.comgit config --global user.email you@example.comm>
2024-01-28 13:44:39 +0530

    initial commit

---
+++ src/app/register/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./page.css";
+
+export default function Register() {
+  return (
+
+    <main className={styles.main}>
+ <div className={styles.circle}></div>
+     <div className={styles.bioVault}>BioVault</div>
+    </main>
+  );
+}
+
+
+  
