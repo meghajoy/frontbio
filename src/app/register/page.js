a9e671c0e86ea377e16a4f299a830fbd77d14817
Your NameMegha <you@example.coaamimeghajoy@gmail.comgit config --global user.email you@example.comm>
2024-01-30 11:52:34 +0530

    register and registering

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import styles from "./page.css";
+import Image from "next/image";
+import styles from "./register.module.css";
 
 export default function Register() {
   return (
-
-    <main className={styles.main}>
- <div className={styles.circle}></div>
-     <div className={styles.bioVault}>BioVault</div>
+    <main>
+      <div className={styles.bioVault}>
+        B<span className={styles.iitalics}>i</span>oVa<span className={styles.letteru}>u</span>
+        <span className={styles.litalics}>l</span>
+        <span className={styles.blackText}>t</span>
+      </div>
+      <div className={styles.circle}>
+        <Image src="/u1.png" width={407} height={501} alt="Fingerprint" />
+      </div>
+      <Image className={styles.finger2}src="/u2.png" width={247} height={380} alt="Fingerprint" />
+      <div className={styles.roundrect}>
+        <span className={styles.reg}>Register your fingerprint</span>
+        <span className={styles.regcircle}>
+          <Image src="/u3.png" width={35} height={45} alt="Fingerprint" />
+        </span>
+      </div>
+      <div className={styles.login}>Login
+        <Image src="/u4.png" width={52} height={52} alt="Fingerprint" />
+      </div>
     </main>
   );
 }
-
-
-  
---
+++ src/app/regnext/page.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import styles from "./regnext.module.css";
+
+export default function RegNext() {
+  return (
+    <main>
+        <div className={styles.rect}>
+        <Image src="/rectnoise.png" width={640} height={836} alt="Fingerprint" />
+
+        <div className={styles.bioVault}>
+        B<span className={styles.iitalics}>i</span>oVa<span className={styles.letteru}>u</span>
+        <span className={styles.litalics}>l</span>
+        <span className={styles.blackText}>t</span>
+        </div>
+        </div>
+        <div className={styles.circle}>
+             <Image src="/Ellipse 10.png" width={350} height={350} alt="Fingerprint" />
+            </div>
+            <Image className={styles.u5finger}src="/u5.png" width={348} height={360} alt="Fingerprint" />
+            <Image className={styles.cufinger}src="/CuntPrint.png" width={40} height={62} alt="Fingerprint" />
+        <span className={styles.register}>Register</span>
+        <div className={styles.linesContainer}>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+        <div className={styles.enter}>
+            <span className={styles.entering}>Name</span>
+            <span className={styles.entering}>Email</span>
+            <span className={styles.entering}>Phone no.</span> 
+            <span className={styles.entering}>Add your fingerprints</span> 
+
+        </div>
+        </div>
+        <div className={styles.outcircle1}></div>
+        <div className={styles.outcircle2}></div>
+        <div className={styles.outcircle3}></div>
+
+        <div className={styles.boxcontainer}>
+            <div className={styles.box}></div>
+            <div className={styles.box}></div>
+            <div className={styles.box}></div>
+        </div>
+        <div className={styles.pluscontainer}>
+          <Image className={styles.plus}src="/plus.png"width={24} height={24} alt="Fingerprint" />
+          <Image className={styles.plus}src="/plus.png"width={24} height={24} alt="Fingerprint" />  
+          <Image className={styles.plus}src="/plus.png"width={24} height={24} alt="Fingerprint" />
+         </div>
+
+    </main>
+  );
+}
